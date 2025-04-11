00d870d9479ede5710fa8140ff0aa4fbaf10ff14
zhangyifeng.114 <zhangyifeng.114@bytedance.com>
2025-04-12 01:43:57 +0800

    finish task1_1

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -22,8 +22,9 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
     Returns:
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
-    # TODO: Implement for Task 1.1.
-    raise NotImplementedError("Need to implement for Task 1.1")
+    df = f(*vals[:arg], vals[arg] + epsilon, *vals[arg + 1 :]) - f(*vals)
+    
+    return df / epsilon
 
 
 variable_count = 1
